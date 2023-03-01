9da5ce46d9e5001f07d767a734c6dc8046f61d1c
Bohdan Tertyshchev <bobterya@gmail.com>
2023-03-01 22:23:30 +0200

    data types

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,5 +1,12 @@
+DROP TABLE users;
+
 CREATE TABLE users(
     first_name VARCHAR(64),
     last_name VARCHAR(64),
-    email text
+    email text,
+    gender VARCHAR(30),
+    is_subscribe boolean,
+    birthday date,
+    foot_size smallint,
+    height numeric(3, 2)
 );
