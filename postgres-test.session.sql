4bd26fc3606aa105f72420f7ef63643c0c77fc5a
Bohdan Tertyshchev <bobterya@gmail.com>
2023-03-01 22:46:06 +0200

    null check

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -1,9 +1,9 @@
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name VARCHAR(64),
-    last_name VARCHAR(64),
-    email text,
+    first_name VARCHAR(64) not NULL,
+    last_name VARCHAR(64) not NULL,
+    email text not NULL,
     gender VARCHAR(30),
     is_subscribe boolean,
     birthday date,
@@ -12,6 +12,9 @@ CREATE TABLE users(
 );
 
 INSERT INTO users VALUES
-('Petr', 'Petrov', 'mail', 'male', true, '1970-09-15', 40, 2.10),
+(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
+INSERT INTO users VALUES
+('Petr', 'Petrov', 'mail', 'male', true, '1970-09-15', NULL, 2.10),
 ('Misha', 'Petrovich', 'mail', 'male', true, '1770-09-15', 20, 2.10),
-('Grisha', 'Petrovskiy', 'mail', 'male', true, '1970-10-15', 50, 2.10);
+('Grisha', 'Petrovskiy', 'mail', 'male', true, '1970-10-15', NULL, 2.10);
