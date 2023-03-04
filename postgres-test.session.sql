c9ed15102c49f16f74e090752a1c79fc2fa0504f
Bohdan Tertyshchev <bobterya@gmail.com>
2023-03-04 19:48:52 +0200

    Data check

--- postgres-test.session.sql
+++ postgres-test.session.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     email text not NULL CHECK(email != '') UNIQUE,
     gender VARCHAR(30),
     is_subscribe boolean,
-    birthday date,
+    birthday date CHECK(birthday < current_date),
     foot_size smallint,
     height numeric(3, 2) CONSTRAINT too_hight_user CHECK(height < 3.0)
 );
@@ -17,4 +17,7 @@ INSERT INTO users VALUES
 INSERT INTO users VALUES
 ('Petr', 'Petrov', 'mail1', 'male', true, '1970-09-15', NULL, 2.10),
 ('Misha', 'Petrovich', 'mail2', 'male', true, '1770-09-15', 20, 2.10),
-('Grisha', '', 'mail3', 'male', true, '1970-10-15', NULL, 2.10);
+('Grisha', 'dfd', 'mail3', 'male', true, '1970-10-15', NULL, 2.10);
+
+INSERT INTO users VALUES
+('Petro', 'Petrov', 'mail1@gmail.com', 'male', true, '2023-03-04', NULL, 2.10),
